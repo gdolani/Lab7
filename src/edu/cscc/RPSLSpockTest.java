926d417cadee8cfb673c4e6ac1808f4fa432be1b
gdolani <gduresso@student.cscc.edu>
2019-11-02 11:55:22 -0400

    Fixed Junit

--- src/edu/cscc/RPSLSpockTest.java
+++ src/edu/cscc/RPSLSpockTest.java
@@ -1,109 +1,24 @@
 package edu.cscc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class RPSLSpockTest {
-    private RPSLSpock rPSLSpock;
+import static org.junit.Assert.*;
 
-    /**
-     * Base RPSLSpockTest class.
-     * @author Girma Duresso
-     */
+public class RPSLSpockTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        rPSLSpock= new RPSLSpock();
+    @Test
+    public void compute() {
     }
 
-    /**
-     *Constructor
-     * @param rPSLSpock computes isValidPick test.
-     */
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @Test
+    public void isValidPick() {
     }
 
-    @org.junit.jupiter.api.Test
-    void compute() {
+    @Test
+    public void generatePick() {
     }
 
-    @org.junit.jupiter.api.Test
-    void isValidPickNull() {
-        assertFalse(rPSLSpock.isValidPick(null));
+    @Test
+    public void isComputerWin() {
     }
-    @org.junit.jupiter.api.Test
-    void isValidPickRock() {
-       assertTrue(rPSLSpock.isValidPick("rock"));
-    }
-    @org.junit.jupiter.api.Test
-    void isValidPickFalse() {
-        assertFalse(rPSLSpock.isValidPick("Girma"));
-    }
-    @org.junit.jupiter.api.Test
-    void isValidPickPaper() {
-        assertTrue(rPSLSpock.isValidPick("paper"));
-    }
-    @org.junit.jupiter.api.Test
-    void isValidPickLizard() {
-        assertTrue(rPSLSpock.isValidPick("lizard"));
-    }
-    @org.junit.jupiter.api.Test
-    void isValidPickScissors() {
-        assertTrue(rPSLSpock.isValidPick("scissors"));
-    }
-    @org.junit.jupiter.api.Test
-    void isValidPickSpock() {
-        assertTrue(rPSLSpock.isValidPick("spock"));
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void generatePick() {
-        for(int i=0;i<1000000;i++) {
-            assertNotNull(rPSLSpock.generatePick());
-        }
-    }
-
-    @org.junit.jupiter.api.Test
-    void isComputerWin1() {
-        assertTrue(rPSLSpock.isComputerWin("rock","lizard"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin2() {
-        assertTrue(rPSLSpock.isComputerWin("rock","scissors"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin3() {
-        assertFalse(rPSLSpock.isComputerWin("paper","scissors"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin4() {
-        assertTrue(rPSLSpock.isComputerWin("paper","spock"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin5() {
-        assertFalse(rPSLSpock.isComputerWin("rock","spock"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin6() {
-        assertTrue(rPSLSpock.isComputerWin("scissors","lizard"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin7() {
-        assertFalse(rPSLSpock.isComputerWin("paper","lizard"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin8() {
-        assertTrue(rPSLSpock.isComputerWin("lizard","spock"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin9() {
-        assertTrue(rPSLSpock.isComputerWin("paper","spock"));
-    }
-    @org.junit.jupiter.api.Test
-    void isComputerWin10() {
-        assertTrue(rPSLSpock.isComputerWin("spock","rock"));
-    }
-
 }
